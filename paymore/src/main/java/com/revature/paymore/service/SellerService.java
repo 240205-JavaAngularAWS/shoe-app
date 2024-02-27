659be507540f333646adc53e43f06aecba97a2ef
Nadida22 <116320424+Nadida22@users.noreply.github.com>
2024-02-27 12:05:43 -0600

    Adding fields to User and seller services

--- paymore/src/main/java/com/revature/paymore/service/SellerService.java
+++ paymore/src/main/java/com/revature/paymore/service/SellerService.java
@@ -1,4 +1,28 @@
 package com.revature.paymore.service;
 
+import com.revature.paymore.repository.SellerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class SellerService {
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+
+    public SellerService(SellerRepository sellerRepository){
+        this.sellerRepository = sellerRepository;
+    }
+
+
+    private static final Logger logger = LoggerFactory.getLogger(SellerService.class);
+
+
+
+
 }
+
--- paymore/src/main/java/com/revature/paymore/service/UserService.java
+++ paymore/src/main/java/com/revature/paymore/service/UserService.java
@@ -1,4 +1,21 @@
 package com.revature.paymore.service;
 
+import com.revature.paymore.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository =userRepository;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 }
