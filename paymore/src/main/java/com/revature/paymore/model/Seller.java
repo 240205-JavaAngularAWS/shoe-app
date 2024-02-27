988a046a14029cc61c4acb0561a063788a893577
Kendra <kendracodesio@gmail.com>
2024-02-27 13:32:23 -0800

    completed Product, Seller, User models

--- paymore/src/main/java/com/revature/paymore/model/Seller.java
+++ paymore/src/main/java/com/revature/paymore/model/Seller.java
@@ -2,6 +2,8 @@ package com.revature.paymore.model;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "sellers")
 public class Seller {
@@ -23,10 +25,92 @@ public class Seller {
     @Column(name = "password")
     private String password;
 
+    @OneToMany(mappedBy = "seller")
     private List<Product> products;
 
     private Seller() {
 
+    }
+
+    public Seller(Long id, String companyName, String email, String username, String password, List<Product> products) {
+        this.id = id;
+        this.companyName = companyName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Seller seller)) return false;
+        return Objects.equals(getId(), seller.getId()) && Objects.equals(getCompanyName(), seller.getCompanyName()) && Objects.equals(getEmail(), seller.getEmail()) && Objects.equals(getUsername(), seller.getUsername()) && Objects.equals(getPassword(), seller.getPassword()) && Objects.equals(getProducts(), seller.getProducts());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getCompanyName(), getEmail(), getUsername(), getPassword(), getProducts());
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", companyName='" + companyName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", products=" + products +
+                '}';
     }
 
 
--- paymore/src/main/java/com/revature/paymore/model/User.java
+++ paymore/src/main/java/com/revature/paymore/model/User.java
@@ -3,6 +3,8 @@ package com.revature.paymore.model;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -27,14 +29,119 @@ public class User {
     private String password;
 
     //a user might have a shipping address and billing address that are different
+    @ManyToMany
+    @JoinTable(
+            name ="user_address",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id")
+    )
+    private List<Address> address;
 
-    private Address address;
-
+    @OneToMany(mappedBy = "user")
     private List<Order> orders;
 
     private User() {
 
     }
 
+    public User(Long id, String firstName, String lastName, String email, String username, String password, List<Address> address, List<Order> orders) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.orders = orders;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<Address> address) {
+        this.address = address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getFirstName(), user.getFirstName()) && Objects.equals(getLastName(), user.getLastName()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getUsername(), user.getUsername()) && Objects.equals(getPassword(), user.getPassword()) && Objects.equals(getAddress(), user.getAddress()) && Objects.equals(getOrders(), user.getOrders());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getEmail(), getUsername(), getPassword(), getAddress(), getOrders());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", address=" + address +
+                ", orders=" + orders +
+                '}';
+    }
 }
