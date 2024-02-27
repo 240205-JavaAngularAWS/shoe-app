d715d9d2ebef11c3a6a265591950defec66b2fef
jojobee333 <symone.j.bates@gmail.com>
2024-02-27 15:24:41 -0500

    Moved user services

--- paymore/src/main/java/service/UserService.java
+++ paymore/src/main/java/service/UserService.java
@@ -1,20 +1,2 @@
 package service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import repository.UserRepository;
-
-@Service
-public class UserService {
-
-    @Autowired
-    UserRepository userRepository;
-
-    public UserService(UserRepository userRepository){
-        this.userRepository =userRepository;
-    }
-
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
-}
