0293df0e874d0e0dac5e2a918daae439e128ebee
Nadida22 <116320424+Nadida22@users.noreply.github.com>
2024-02-27 10:31:06 -0600

    pushing spring initializer

---
+++ paymore/src/main/java/com/revature/paymore/PaymoreApplication.java
@@ -0,0 +1,13 @@
+package com.revature.paymore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymoreApplication.class, args);
+	}
+
+}
---
+++ paymore/src/test/java/com/revature/paymore/PaymoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.paymore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
