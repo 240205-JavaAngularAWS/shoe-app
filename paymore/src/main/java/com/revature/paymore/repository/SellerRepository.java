0666068379c7cc619e360f31b5070c14a0c4507a
Kendra <kendracodesio@gmail.com>
2024-02-27 17:28:49 -0800

    adds methods registerSeller and authenticateSeller; adds globalexceptionhandler

--- paymore/src/main/java/com/revature/paymore/repository/SellerRepository.java
+++ paymore/src/main/java/com/revature/paymore/repository/SellerRepository.java
@@ -4,7 +4,15 @@ import com.revature.paymore.model.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+    Optional<Seller> findByUsername(String username);
+
+    Optional<Seller> findByUsernameAndPassword(String username, String password);
+
+
 }
