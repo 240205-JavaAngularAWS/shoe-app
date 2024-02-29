e175c735b768d74aac87027c399faf485e2e6714
Nadida22 <116320424+Nadida22@users.noreply.github.com>
2024-02-29 00:43:01 -0600

    implemented adding/removing product service and controller

--- paymore/src/main/java/com/revature/paymore/controller/ProductController.java
+++ paymore/src/main/java/com/revature/paymore/controller/ProductController.java
@@ -1,6 +1,46 @@
 package com.revature.paymore.controller;
+import com.revature.paymore.exception.BadRequestException;
+import com.revature.paymore.model.DTO.ProductDTO;
+import com.revature.paymore.model.Product;
+import com.revature.paymore.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+
+    // adding Products
+    @PostMapping("/products")
+    public ResponseEntity<?> addProducts(@RequestBody Product product){
+
+        ProductDTO addedProduct = productService.addProducts(product);
+        return new ResponseEntity<>(addedProduct, HttpStatus.OK);
+    }
+
+
+    // deleting Products
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProducts(@PathVariable Long productId){
+
+        boolean deleted = productService.deleteProduct(productId);
+        if(!deleted){
+            throw new BadRequestException("Product with id " + productId + " was not found.");
+        }
+        return new ResponseEntity<>("{\"message\":\"Successfully Deleted\"}", HttpStatus.OK);
+    }
+
 }
--- paymore/src/main/java/com/revature/paymore/service/ProductService.java
+++ paymore/src/main/java/com/revature/paymore/service/ProductService.java
@@ -1,11 +1,22 @@
 package com.revature.paymore.service;
 
+import com.revature.paymore.exception.BadRequestException;
+import com.revature.paymore.exception.GlobalExceptionHandler;
+import com.revature.paymore.model.DTO.ProductDTO;
 import com.revature.paymore.model.Product;
+import com.revature.paymore.model.Seller;
+import com.revature.paymore.model.enums.Category;
+import com.revature.paymore.model.enums.Gender;
 import com.revature.paymore.repository.ProductRepository;
 import com.revature.paymore.repository.ReviewRepository;
 import com.revature.paymore.repository.SellerRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ProductService {
@@ -14,18 +25,51 @@ public class ProductService {
     ProductRepository productRepository;
 
     @Autowired
-    SellerRepository sellerRepository
+    SellerRepository sellerRepository;
 
     public ProductService(ProductRepository productRepository, SellerRepository sellerRepository) {
         this.productRepository = productRepository;
         this.sellerRepository = sellerRepository;
     }
 
-    public Product addProducts(Product product){
+    private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
 
 
-        return null;
+
+    public ProductDTO addProducts(Product product){
+
+        // check if seller exists
+        Seller activeSeller = sellerRepository.findById(product.getSeller().getId())
+                .orElseThrow(() -> new EntityNotFoundException( " This product does not have any Seller " ));
+
+        // check if price and quantity is greater than 0
+        if(product.getPrice() < 0 && product.getQuantity() < 0){
+            throw new BadRequestException("Product's price or quantity is not valid");
+        }
+
+        // check if user inputs a valid gender and category
+        if(product.getGender() != Gender.MENS && product.getGender() != Gender.WOMENS){
+            throw new BadRequestException("Product gender input is invalid");
+        }
+
+        if(product.getCategory() != Category.ATHLETIC && product.getCategory() != Category.CASUAL && product.getCategory() != Category.DRESS){
+            throw new BadRequestException("Product category input is invalid");
+        }
+
+        productRepository.save(product);
+        return new ProductDTO(product);
     }
+
+
+    public boolean deleteProduct(long productId){
+
+        return productRepository.findById(productId)
+                                .map(user -> { productRepository.deleteById(productId);
+                                return true;})
+                                .orElse(false);
+    }
+
+
 
 
 }
