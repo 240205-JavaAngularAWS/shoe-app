54507260332881f279732ca6bd39ba8e10e8632f
Nadida22 <116320424+Nadida22@users.noreply.github.com>
2024-02-28 21:46:41 -0600

    merging changes from working branch

--- paymore/src/main/java/com/revature/paymore/service/ProductService.java
+++ paymore/src/main/java/com/revature/paymore/service/ProductService.java
@@ -1,6 +1,9 @@
 package com.revature.paymore.service;
 
+import com.revature.paymore.model.Product;
 import com.revature.paymore.repository.ProductRepository;
+import com.revature.paymore.repository.ReviewRepository;
+import com.revature.paymore.repository.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +13,18 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository){
+    @Autowired
+    SellerRepository sellerRepository
+
+    public ProductService(ProductRepository productRepository, SellerRepository sellerRepository) {
         this.productRepository = productRepository;
+        this.sellerRepository = sellerRepository;
+    }
+
+    public Product addProducts(Product product){
+
+
+        return null;
     }
 
 
